ad0a287973e132feca6faa614313cadccbd6a234
tusharind <tusharj3255@gmail.com>
2025-07-23 00:23:55 +0530

    initial commit

--- Week2.1/ViewController.swift
+++ Week2.1/ViewController.swift
@@ -1,19 +1,34 @@
-//
-//  ViewController.swift
-//  Week2.1
-//
-//  Created by Coditas on 22/07/25.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var versionLabel: UILabel!
+    @IBOutlet weak var bundleIdLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureLabels()
     }
 
+    private func configureLabels() {
+        let allLabels = [nameLabel, versionLabel, bundleIdLabel]
 
+        for label in allLabels {
+            label?.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+            label?.numberOfLines = 0
+            label?.lineBreakMode = .byWordWrapping
+            label?.textAlignment = .left
+        }
+
+        nameLabel.text = "App Name: Project Explorer"
+
+        if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            versionLabel.text = "Version: \(version)"
+        } else {
+            versionLabel.text = "Version: 1.0"
+        }
+
+        bundleIdLabel.text = "Bundle ID: \(Bundle.main.bundleIdentifier ?? "com.yourcompany.ProjectExplorer")"
+    }
 }
-
