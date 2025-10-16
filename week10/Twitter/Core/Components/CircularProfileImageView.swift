d8d219b43d280f21cfd8ce2ef25c3e6ce79adf84
coditas <coditas@5824-LAP-0880.local>
2025-10-16 12:36:31 +0530

    twitter proj added

---
+++ week10/Twitter/Core/Components/CircularProfileImageView.swift
@@ -0,0 +1,27 @@
+import Kingfisher
+import SwiftUI
+
+struct CircularProfileImageView: View {
+    var user: User?
+
+    init(user: User?) {
+        self.user = user
+    }
+
+    var body: some View {
+        if let imageURL = user?.profileImageURL {
+            KFImage(URL(string: imageURL))
+                .resizable()
+                .scaledToFill()
+                .frame(width: 40, height: 40)
+                .clipShape(Circle())
+
+        } else {
+            Image(systemName: "person.fill")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 40, height: 40)
+                .clipShape(Circle())
+        }
+    }
+}
---
+++ week10/Twitter/Core/Profile/CurrentUserProfile/View/CurrentUserProfileView.swift
@@ -0,0 +1,71 @@
+import SwiftUI
+
+struct CurrentUserProfileView: View {
+    @StateObject var viewModel = CurrentUserProfileViewModel()
+    @State private var showEditProfile = false
+
+    private var currentUser: User? {
+        Task { try await UserService.shared.fetchCurrentUser() }
+        return viewModel.currentUser
+    }
+    var body: some View {
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                //bio and stats
+                VStack(spacing: 20) {
+
+                    ProfileHeaderView(user: currentUser)
+
+                    Button {
+                        showEditProfile.toggle()
+                    } label: {
+                        Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.black)
+                            .frame(width: 352, height: 32)
+                            .background(.white)
+                            .cornerRadius(8)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color(.systemGray6), lineWidth: 1)
+                            )
+                    }
+                    if let user = currentUser {
+                        UserContentListView(user: user)
+                    }
+
+                }
+            }
+            .sheet(
+                isPresented: $showEditProfile,
+                content: {
+                    if let user = currentUser {
+                        EditProfileView(user: user)
+                    }
+
+                }
+            )
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Button {
+                        AuthService.shared.signOut()
+                    } label: {
+                        Text("Sign Out")
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)  // text color
+                            .padding(.horizontal, 16)
+                            .padding(.vertical, 8)
+                            .background(Color.black)  // button background
+                            .clipShape(Capsule())
+                    }
+                }
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+#Preview {
+    CurrentUserProfileView()
+}
