b4872ef198b62c34d93829b10bb71708ba674eb9
coditas <coditas@5824-LAP-0880.local>
2025-10-22 10:05:04 +0530

    refractored code

--- week14/SocialSignIns/SocialSignIns/Storage/AppUser.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import FirebaseFirestore
-
-struct AppUser: Codable, Identifiable {
-    @DocumentID var id: String?            
-    var email: String?
-    var displayName: String?
-}
